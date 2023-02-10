9e34e3e51c1286df6706552d2de4b7f25a8887fe
Aditya79000 <105648681+Aditya79000@users.noreply.github.com>
2023-02-10 18:47:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+song="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload()
+{
+}
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.position(600, 300);
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
+}
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
